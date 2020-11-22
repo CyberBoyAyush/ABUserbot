169a4f9645c192f60b484deb2158be96566c2b36
Ayush Sharma <aayush240204@gmail.com>
2020-11-22 13:22:47 +0530

    Plugins😍️😍️

---
+++ userbot/plugins/app.py
@@ -0,0 +1,96 @@
+"""Fetch App Details from Playstore. @AyushBots 🚶
+.app <app_name> to fetch app details.
+.appr <app_name>  to fetch app details with Xpl0iter request link.
+.mods <app_name> to get the premier app from telegram if available.."""
+
+import requests
+
+import bs4
+
+import re
+
+
+
+from telethon import *
+
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from var import Var
+from userbot.events import register
+
+
+@borg.on(events.NewMessage(pattern='.app (.*)'))
+
+@borg.on(events.MessageEdited(pattern='.app (.*)'))
+
+async def apk(e):
+    try:
+        app_name = e.pattern_match.group(1)
+        remove_space = app_name.split(' ')
+        final_name = '+'.join(remove_space)
+        page = requests.get("https://play.google.com/store/search?q="+final_name+"&c=apps")
+        lnk = str(page.status_code)
+        soup = bs4.BeautifulSoup(page.content,'lxml', from_encoding='utf-8')
+        results = soup.findAll("div","ZmHEEd")
+        app_name = results[0].findNext('div', 'Vpfmgd').findNext('div', 'WsMG1c nnK0zc').text
+        app_dev = results[0].findNext('div', 'Vpfmgd').findNext('div', 'KoLSrc').text
+        app_dev_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('a', 'mnKHRc')['href']
+        app_rating = results[0].findNext('div', 'Vpfmgd').findNext('div', 'pf5lIe').find('div')['aria-label']
+        app_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('div', 'vU6FJ p63iDd').a['href']
+        app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
+        app_details = "<a href='"+app_icon+"'>ЁЯУ▓&#8203;</a>"
+        app_details += " <b>"+app_name+"</b>"
+        app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
+        app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "тнР ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "тнР ").replace("five", "5")
+        app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
+        app_details += "\n\n===> HellBot <==="
+        await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
+    except IndexError:
+        await e.edit("No result found in search. Please enter **Valid app name**")
+    except Exception as err:
+        await e.edit("Exception Occured:- "+str(err))
+
+@borg.on(events.NewMessage(pattern='.appr (.*)'))
+
+@borg.on(events.MessageEdited(pattern='.appr (.*)'))
+
+async def apkr(e):
+    try:
+        app_name = e.pattern_match.group(1)
+        remove_space = app_name.split(' ')
+        final_name = '+'.join(remove_space)
+        page = requests.get("https://play.google.com/store/search?q="+final_name+"&c=apps")
+        lnk = str(page.status_code)
+        soup = bs4.BeautifulSoup(page.content,'lxml', from_encoding='utf-8')
+        results = soup.findAll("div","ZmHEEd")
+        app_name = results[0].findNext('div', 'Vpfmgd').findNext('div', 'WsMG1c nnK0zc').text
+        app_dev = results[0].findNext('div', 'Vpfmgd').findNext('div', 'KoLSrc').text
+        app_dev_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('a', 'mnKHRc')['href']
+        app_rating = results[0].findNext('div', 'Vpfmgd').findNext('div', 'pf5lIe').find('div')['aria-label']
+        app_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('div', 'vU6FJ p63iDd').a['href']
+        app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
+        app_details = "<a href='"+app_icon+"'>ЁЯУ▓&#8203;</a>"
+        app_details += " <b>"+app_name+"</b>"
+        app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
+        app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "тнР ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "тнР ").replace("five", "5")
+        app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
+        app_details += "\n\n<b>Download : </b> <a href='https://t.me/HellBot_Official'>Request_Here</a>"
+        app_details += "\n\n===> HellBot <==="
+        await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
+    except IndexError:
+        await e.edit("No result found in search. Please enter **Valid app name**")
+    except Exception as err:
+        await e.edit("Exception Occured:- "+str(err))
+
+
+@borg.on(admin_cmd(pattern="mods ?(.*)"))
+async def mod(event):
+    if event.fwd_from:
+        return
+    modr = event.pattern_match.group(1)
+    botusername = "@PremiumAppBot"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, modr) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
